ef13fd02caa4f482ca2094f38021bb20a6fa9e5d
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-02 13:01:06 +0500

    Add files via upload
    string practise

---
+++ chapter 5 practise/formet string practice.py
@@ -0,0 +1,9 @@
+# 1: If x = 9, what is a correct syntax to print 'The price is 9.00 dollars'?
+x = 9
+txt = f" The price is {x:.2f} dollar"
+print(txt)
+
+# 2: What will be the result of the following code:
+a = 2
+b = 3
+print(f'The price is {2 + 3} dollars')
---
+++ chapter 5 practise/modify python ptactise.py
@@ -0,0 +1,14 @@
+# 1: What is a correct syntax to print a string in upper case letters?
+a = 'welcome'
+print('Welcome'.upper())
+
+# 2: Return the string without any whitespace at the beginning or the end.
+txt = " Hello World "
+x =  txt.strip()
+print(x)
+
+# 3: What is a correct syntax to print a string in lower case letters?
+a = 'welcome'
+b ='Welcome'.lower()
+print(b)
+
