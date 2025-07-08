6fe239ae46ed81d1d30d5dad38556c5cb0343f21
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-08 16:33:36 +0500

    Add files via upload
    Operator

---
+++ chapter 7 operator/python o[erator.py
@@ -0,0 +1,61 @@
+operator are the operation that are perfrom on varibale ans value
+1:Arithmetic operators
+2:Assignment operators
+3:Comparison operators
+4:Logical operators
+5:Identity operators
+6:Membership operators
+7:Bitwise operators
+
+***********************************************ARTHIMETIC OPERTORE(+,-,*,/,**,%,//)************************
+a = 4
+b = 3
+print (a+b)   #addition
+print(a-b)    #subsration
+print(a*b)    #multiplication
+print(a/b)    #division
+print(a%b)    #module
+print(a**b)   #exponansion
+print(a//b)   # floor division
+
+*****************************ASSIGMENT OPERRATOR(=,+=,-=,*=,**=,%=,/=,//=,<)**********************************
+x=5
+print(x =x + 3)	
+print(x = x - 3	)
+print(x = x * 3	)
+print (x = x / 3)
+print(x = x % 3	)
+print(x = x // 3)	
+print(x = x ** 3)	
+print(x = x & 3	)
+print(x = x | 3	)
+print(x = x ^ 3)
+print(x = x >> 3)	
+print(x = x << 3)	
+print(x := 3)	
+print(x)
+
+
+
+# ***************************************PYTHON CAMPARISION OPERATOR(==,!=,>,<,>=,,=)*************************************
+x = 3
+y = 5
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+# ****************************************PYTHON LOGICAL OPERATOR(NOT,OR,AND)**************************************************** 
+x = 4
+print(x < 5 and  x < 10)
+print(x < 5 or x < 4)
+print(not(x < 5 and x < 10))
+
+# ***************************************Python Identity Operators(is,is not)
+x = 3
+y = 4
+print(x is y)
+print(x is not y)
+
