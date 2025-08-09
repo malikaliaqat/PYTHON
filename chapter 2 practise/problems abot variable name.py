82ec76df5fcc78324c757dcba591a6ca9cd6a430
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-08-09 15:13:50 +0500

    Delete chapter 2 practise directory

--- chapter 2 practise/problems abot variable name.py
+++
@@ -1,8 +0,0 @@
-# 1: what is not allowed ornot legall in python by writing a variable
-my-var = 20
-2myvar = "John"
-my var = "John"
-
-# 2: Create a variable named carname and assign the value Volvo to it.
-carname = "Volvo"
-print(carname)
