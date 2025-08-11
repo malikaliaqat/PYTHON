2cb8f2108b503fcc4039c848e371510f63b75e71
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-08-11 16:20:32 +0500

    Delete module.py

--- module.py
+++
@@ -1,9 +0,0 @@
-
-import pyjokes
-joke = pyjokes.get_joke()
-print(joke) 
- 
-#  comment
-# single line comment
-""" This  sign is for writing a multi line comment"""
-# print("Hello, World!")  # This is a comment
