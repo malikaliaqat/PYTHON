3d15100d02f68650fedd27a0660299d4886811ac
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-08-12 13:36:19 +0500

    Add files via upload

---
+++ chapter2 variable/Assign multiple value.py
@@ -0,0 +1,14 @@
+# assiging multple value in one line
+a,b,c = 1,11,4
+print(a,b,c)  # output: 1 11 4
+x,y,z = "malika" ,"dua", "soha"
+print(x,y,z)  # output: malika dua soha
+
+# assiging single value in one line
+x = y = z = "Orange"
+print(x,y,z)    # output: Orange Orange Orange
+
+#  extract the values  from list into variables
+fruit = ["apple","banana","cherry"]
+x,y,z = fruit
+print(fruit,x)
