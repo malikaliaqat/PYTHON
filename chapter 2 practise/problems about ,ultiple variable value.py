29edd3bb2bddf89b1330c69978a4b4a694934105
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-08-12 13:37:26 +0500

    Add files via upload

---
+++ chapter 2 practise/problems about ,ultiple variable value.py
@@ -0,0 +1,18 @@
+# 1: What is a correct syntax to add the value 'Hello World', to 3 variables in one statement?
+x=y=z = "hello world"
+print(x)
+print(y)
+print(z)
+
+# 2: Insert the correct syntax to assign values to multiple variables in one line:
+x, y, z = "hello", "world", "python"
+print(x)
+print(y)
+print(z)
+
+# 3:  print the vaue of a b c
+fruits = ['apple', 'banana', 'cherry']
+a, b, c = fruits
+print(a)
+print(b)
+print(c)
