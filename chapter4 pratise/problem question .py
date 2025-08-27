9f017044b85bbd535714591529083c5a3e64b16e
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-08-27 21:24:00 +0500

    Add files via upload

---
+++ chapter4 pratise/problem question .py
@@ -0,0 +1,6 @@
+# 1 : Which is NOT a legal numeric data type in Python:
+
+x = 1.0            # Legal numeric data type: float
+y = 1              # Legal numeric data type: int
+z = long(234)      # not legal
+print (x, y,z)     # Output: 1.0 1 1
