250f8ba79f84226781d33309d65ebba3418355e2
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-09-24 16:59:43 +0500

    Add files via upload

---
+++ chapter 9 Tuple/if-else statement.py
@@ -0,0 +1,12 @@
+#  1: Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+# Output: Yes, 'apple' is in the fruits tuple
+
+# 2: if "orange" is not present in the tuple:
+thistuple = ("apple", "banana", "cherry")
+if "orange"  not in thistuple:
+  print("Yes, 'orange' is  not in the fruits tuple")
+# Output: Yes, 'orange' is  not in the fruits tuple
