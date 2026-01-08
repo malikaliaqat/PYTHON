d2f3ec61b53f30b62eb913acf1b7317978f84a88
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-01-08 23:09:43 +0500

    Delete chapter 19 lambda directory

--- chapter 19 lambda/use of lambda.py
+++
@@ -1,8 +0,0 @@
-# why ise of lambda 
-#  because lambdais better shon when you use then as anonymous function inside another function
-def function(n):
-   return lambda a : a * n
-m = function(5)
-n = function(6)
-print(m(6))
-print(n(4))
