4d3d3a97707d38a63c89715f39dc52a9b5749af2
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-04-27 01:20:29 -0700

    Add files via upload

---
+++ chapter 5 string/concatenation string.py
@@ -0,0 +1,10 @@
+# To concatenate, or combine, two strings you can use the + operator.
+a = "malika"
+b = "liaqat"
+c =  a + b
+print(c)    # output = "malikaliaqat"
+
+a ="malika "
+b = "liaqat"
+c = a + "" + b
+print(c)    # output = "malika liaqat"
