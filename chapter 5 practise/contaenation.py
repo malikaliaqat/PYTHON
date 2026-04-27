8a04ce5416c3c7359c1a658ad33fff3b4e3a47b5
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-04-27 01:21:06 -0700

    Add files via upload

---
+++ chapter 5 practise/contaenation.py
@@ -0,0 +1,19 @@
+# 1: What is a correct syntax to merge variable x and y into variable z?
+x = 4
+y = 5
+z = x + y
+print(z)
+
+# 2: What will be the result of the following code:
+x = 'Welcome'
+y = 'Coders'
+print(x + y)
+
+# 3:What is a correct syntax to print 'Join the party'?
+a = 'Join'
+b = 'the'
+c = 'party'
+z = a + ' ' + b + ' ' + c
+print(z)
+
+
