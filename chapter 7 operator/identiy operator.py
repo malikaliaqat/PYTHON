8b67642bc44544345e5f1412f328b35baf6d06aa
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-04-28 10:17:02 -0700

    Add files via upload

---
+++ chapter 7 operator/identiy operator.py
@@ -0,0 +1,6 @@
+
+# ***************************************Python Identity Operators(is,is not)
+x = 3
+y = 4
+print(x is y)
+print(x is not y)
---
+++ chapter 7 operator/logical operaotor.py
@@ -0,0 +1,6 @@
+
+# ****************************************PYTHON LOGICAL OPERATOR(NOT,OR,AND)**************************************************** 
+x = 4
+print(x < 5 and  x < 10)
+print(x < 5 or x < 4)
+print(not(x < 5 and x < 10))
