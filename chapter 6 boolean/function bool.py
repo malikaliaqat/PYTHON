ec776be7b494c9dcd6b691805e28f3d1198eed0d
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-04-28 07:29:19 -0700

    Add files via upload

---
+++ chapter 6 boolean/function bool.py
@@ -0,0 +1,15 @@
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+#  function return boolean value
+def myFunction() :
+  return True
+print(myFunction())    # output = true
+
+def myFunction():
+  return False
+print (myFunction())    # output = false
