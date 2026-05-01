9916fcbe968d861e8a23782f2e431ff35d480a08
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-05-01 10:30:49 -0700

    Add files via upload

---
+++ chapter 13 if-else/if-else statement.py
@@ -0,0 +1,30 @@
+'''we are going to study if else statements 
+we use the if else statement to check whatever the expression is true or false
+ Basic condition statements are futher divide in following type
+1: if
+2: if-else
+3: if-else-elif
+4: nested if-else-elif'''
+
+# ********************************************************if-else*************************************************************
+# 1: AND condition
+a = 6
+b = 4
+c = 4
+# Check conditions using logical operators
+if a > b and a > c:
+    print("a is geater then b amd a is greater then c")
+else:
+    print (" a is eqal to a and b and c")
+
+# 2: OR condition
+a = 6
+b = 4
+c = 4
+# Check conditions using logical operators
+if a > b or  a < c:
+    print("a is geater then b amd a is greater then c")
+else:
+    print (" a is eqal to a and b and c")
+
+
---
+++ chapter 13 if-else/nested -if-else.py
@@ -0,0 +1,9 @@
+# ***********************************************nested - if-else******************************************************
+# 1:
+age = 20
+if age >= 18:
+    print("You are an adult.")
+    if age >= 21:
+        print("You can drink alcohol.")
+else:
+    print("You are a minor.")
