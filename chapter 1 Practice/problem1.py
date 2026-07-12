a72f5c0478f8d6208e1c5f85e7b50e9095501db3
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-12 22:51:59 +0500

    Delete chapter 1 Practice directory

--- chapter 1 Practice/problem1.py
+++
@@ -1,7 +0,0 @@
-# print the frist four line of twinkel twinkel littel star in the python
-print('''Twinkle twinkle little star.
-How I wonder what you are.
-Up above the world so high.
-Like a diamond in the sky''')
-
-
--- chapter 1 Practice/problem2.py
+++
@@ -1,5 +0,0 @@
-#  Install the external module and use in the operator
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("myself malika liaqat and im study in islamiya colg")
-engine.runAndWait()
