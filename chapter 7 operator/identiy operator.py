45d486c7776ee3c414d4de501be39124490b0bc5
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-20 16:46:56 +0500

    Delete chapter 7 operator directory

--- chapter 7 operator/identiy operator.py
+++
@@ -1,6 +0,0 @@
-
-# ***************************************Python Identity Operators(is,is not)
-x = 3
-y = 4
-print(x is y)
-print(x is not y)
--- chapter 7 operator/logical operaotor.py
+++
@@ -1,6 +0,0 @@
-
-# ****************************************PYTHON LOGICAL OPERATOR(NOT,OR,AND)**************************************************** 
-x = 4
-print(x < 5 and  x < 10)
-print(x < 5 or x < 4)
-print(not(x < 5 and x < 10))
