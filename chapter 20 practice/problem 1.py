fd58ea20dbbc66b229e94232078fcbc33cca3e9f
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-22 01:50:46 +0500

    Delete chapter 20 practice directory

--- chapter 20 practice/problem 1.py
+++
@@ -1,7 +0,0 @@
-# write a program to read the text from a given file 'poem.txt 'and find out whether it contain the word 'twinkele'
-with open ("poem.txt") as f:
-    content = f.read()
-    if "twinkle" in content:
-        print("yes")
-    else:
-        print("no")
