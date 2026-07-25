318e52c33eb5bd2bfc4890c2f31f4dcd06d62cf5
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-25 08:09:34 -0700

    Add files via upload

---
+++ chapter 1 module ,comment/module.py
@@ -0,0 +1,27 @@
+
+# # import pyjokes
+# # joke = pyjokes.get_joke()
+# # print(joke) 
+ 
+# # #  comment
+# # # single line comment
+# # """ This  sign is for writing a multi line comment"""
+# # # print("Hello, World!")  # This is a comment
+
+# if 0.3+0.4 ==0.7:
+#     print("True")
+# else:
+#     print("False")
+# i= int(input("enter you number"))
+# while i<=20:
+#   print(i)
+#   i=i+1
+# i=int(input("enter ur number"))
+# while i >= 5:
+#     print(i)
+#     i=i-1
+squares = [x**2 for x in range(5)]
+print(squares)
+
+for x in range(5):
+  print("malika")
