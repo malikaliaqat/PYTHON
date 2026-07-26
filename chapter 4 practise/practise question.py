bc8644ac04e5f190aac83e0c1f42e134f603eada
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-26 10:31:31 -0700

    Add files via upload

---
+++ chapter 4 practise/practise question.py
@@ -0,0 +1,22 @@
+# conversion
+
+# 1: convert the INT into FLOAT
+
+x = 5
+a = float(x)
+print(a)
+
+# 2: convert FLOAT into INT
+x = 2.4
+b= int (x)
+print(b)
+
+# 3: convert INT into COMPLEX
+x = 5
+c = complex(x)
+print(c)
+
+# 4: convert FLOAT into COMPLEX  
+x = 2.3
+d = complex(x)
+print(d)
