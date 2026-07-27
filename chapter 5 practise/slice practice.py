9ae4ddd02d313b79ae3ec93e067fbf77fa9c674f
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-27 06:41:41 -0700

    Add files via upload

---
+++ chapter 5 practise/slice practice.py
@@ -0,0 +1,11 @@
+# 1: What will be the result of the following code:
+x = 'Welcome'
+print(x[3:5])
+
+# 2: Get the characters from index 2 to index 4 (llo).
+txt = "Hello World"
+x =txt[2:5]
+
+# 3: What will be the result of the following code:
+x = 'Welcome'
+print(x[3:])
