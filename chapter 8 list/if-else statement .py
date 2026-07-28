8a75c33a0c2ed1829dd8b54de6d1ab387d54ef77
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-28 03:14:13 -0700

    Add files via upload

---
+++ chapter 8 list/if-else statement .py
@@ -0,0 +1,14 @@
+
+#  6  :using if stsement
+list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+if " kiwi" in list:
+    print("kiwi is present in the list")
+else:
+    print("kiwi is not present in the list")
+
+
+# 7: for no not 
+if "kiwi" not in list:
+   print("kiwi is not present in the list")
+else:
+  print("kiwi is present in the list")
---
+++ chapter 8 list/list .py
@@ -0,0 +1,27 @@
+#  list are use to store multiple item in a single varaiable
+#  list are the built-in data type in python
+
+
+# ***************************************METHODS OF LIST*****************************************
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+# *************************************************************************************************
+
+
+
+#  create list
+list = ["apple ,banana", "cherry", "date"]
+print(list)
+#  output: ["apple ,banana", "cherry", "date"]
+
+#  find the lenght of list
+print(len(list))  # Output: 3
