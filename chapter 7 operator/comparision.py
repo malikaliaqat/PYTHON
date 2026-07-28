253c2a24a8873cc8f8c77218d9096e2ccfd23660
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-07-28 03:11:18 -0700

    Add files via upload

---
+++ chapter 7 operator/comparision.py
@@ -0,0 +1,10 @@
+
+# ***************************************PYTHON CAMPARISION OPERATOR(==,!=,>,<,>=,,=)*************************************
+x = 3
+y = 5
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
---
+++ chapter 7 operator/python operator.py
@@ -0,0 +1,17 @@
+# operator are the operation that are perfrom on varibale ans value
+# 1:Arithmetic operators
+# 2:Assignment operators
+# 3:Comparison operators
+# 4:Logical operators
+# 5:Identity operators
+# 6:Membership operators
+# 7:Bitwise operators
+
+
+
+
+
+
+
+
+
