97503dfd377e48eb955c61495d52499c8652b80b
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-08-02 07:54:52 -0700

    Add files via upload

---
+++ chapter 15 while loop/even number.py
@@ -0,0 +1,19 @@
+#  Using while loop to print even numbers
+# i = 0
+# while i < 10:
+#         i= i + 1
+#         if i % 2 !=0:
+#             continue;   
+#         print(i)
+
+
+#  Number from user to check whatever number is even or odd
+i = int(input("Enter a number: "))
+
+while True:
+    if i % 2 == 0:
+        print(i, "is an even number")
+        print(f"{i} is divisible by 2")
+    else:
+        print(i, "is an odd number")
+    break 
