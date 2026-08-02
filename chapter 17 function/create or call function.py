55d71f5c446140fcd124ac7a5f5c3049bf8a9b67
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-08-02 07:57:19 -0700

    Add files via upload

---
+++ chapter 17 function/create or call function.py
@@ -0,0 +1,13 @@
+# *************************Creating a Function***********************
+# In python a function is defined with the def keyword
+# for example
+# 1:
+def greet(name):
+    print("Hello, " + name + "!")
+# *****************Calling the function*********************
+greet("malika")
+
+# 2:
+def my_function():
+    print ("this is my function")
+my_function()
---
+++ chapter 17 function/return value.py
@@ -0,0 +1,17 @@
+# to let the functiion to return values we use return values
+# 1:
+def function(x):
+     print (5*x)
+# print(function(3))   # prints 15
+result = function(3)
+print("Result is:", result)
+#  this reurn nothing just print the value
+
+# 2:
+def function (x):
+     return 4*x
+print (function(3))
+result = function(3)
+print (result)
+
+
