7482bed8cc45d17dcb4361927314c4239dd661ff
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-08-04 08:44:42 -0700

    Add files via upload

---
+++ chapter 19 practise/question 1 .py
@@ -0,0 +1,18 @@
+# what will the result if the following code :
+x = lambda a : a - 10
+print (x(30))
+# output  20 
+
+
+x = lambda a : a * 10
+print (x(3))
+# output 30
+
+
+x = lambda  a : a + 10
+print (x(3))
+# output 13
+
+
+
+
---
+++ chapter 19 practise/question 4 .py
@@ -0,0 +1,4 @@
+# finding the avg
+def avg (x,y):
+  return (x+y)/2
+print(avg())
